51e6ccee29a2b6e67c2cdd9dd5b25f0322033e63
Marvell69 <adewunmimarvel@gmail.com>
2024-02-01 16:10:36 +0000

    version1

---
+++ js/index.js
@@ -0,0 +1,133 @@
+const start_btn = document.querySelector(".start-btn button");
+const info_box = document.querySelector(".info-box");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = document.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz-box");
+const timeCount = quiz_box.querySelector(".timer .timer-sec");
+
+const option_list = document.querySelector(".option-list");
+
+start_btn.onclick = () => {
+  info_box.classList.add("activeInfo");
+};
+
+exit_btn.onclick = () => {
+  info_box.classList.remove("activeInfo");
+};
+
+continue_btn.onclick = () => {
+  info_box.classList.remove("activeInfo");
+  quiz_box.classList.add("activeQuiz");
+  showQuestions(0);
+  queCounter(1);
+  startTimer(15);
+};
+
+let que_count = 0;
+let que_numb = 1;
+let counter;
+let timeValue = 15;
+
+const next_btn = quiz_box.querySelector(".next-btn");
+
+next_btn.onclick = () => {
+  if (que_count < questions.length - 1) {
+    que_count++;
+    que_numb++;
+    showQuestions(que_count);
+    queCounter(que_numb);
+    clearInterval(counter);
+    startTimer(timeValue);
+  } else {
+    console.log("Questions completed");
+  }
+};
+
+function showQuestions(index) {
+  const que_text = document.querySelector(".que-text");
+
+  let que_tag =
+    "<span>" +
+    questions[index].num +
+    " . " +
+    questions[index].question +
+    "</span>";
+  let option_tag =
+    '<div class="option">' +
+    questions[index].options[0] +
+    "<span></span></div>" +
+    '<div class="option">' +
+    questions[index].options[1] +
+    "<span></span></div>" +
+    '<div class="option">' +
+    questions[index].options[2] +
+    "<span></span></div>" +
+    '<div class="option">' +
+    questions[index].options[3] +
+    "<span></span></div>";
+
+  que_text.innerHTML = que_tag;
+  option_list.innerHTML = option_tag;
+  const option = option_list.querySelectorAll(".option");
+  for (let i = 0; i < option.length; i++) {
+    option[i].setAttribute("onClick", "optionSelected(this)");
+  }
+}
+
+let tickIcon = '<div class="icon-tick"><i class="fas fa-check"></i></div>';
+let crossIcon = '<div class="icon-cross"><i class="fas fa-times"></i></div>';
+
+function optionSelected(answer) {
+  clearInterval(counter);
+
+  let userAns = answer.textContent;
+  let correctAns = questions[que_count].answer;
+  let allOptions = option_list.children.length;
+  if (userAns == correctAns) {
+    answer.classList.add("correct");
+    console.log("Answer is correct");
+    answer.insertAdjacentHTML("beforeend", tickIcon);
+  } else {
+    answer.classList.add("incorrect");
+    console.log("Answer is wrong");
+    answer.insertAdjacentHTML("beforeend", crossIcon);
+
+    for (let i = 0; i < allOptions; i++) {
+      if (option_list.children[i].textContent == correctAns) {
+        option_list.children[i].setAttribute("class", "option correct");
+        option_list.children[i].insertAdjacentHTML("beforeend", tickIcon);
+      }
+    }
+  }
+
+  for (let i = 0; i < allOptions; i++) {
+    option_list.children[i].classList.add("disabled");
+  }
+}
+
+function startTimer(time) {
+  counter = setInterval(timer, 1000);
+  function timer() {
+    timeCount.textContent = time;
+    time--;
+    if (time < 9) {
+      let addZero = timeCount.textContent;
+      timeCount.textContent = "0" + addZero;
+    }
+    if (time < 0) {
+      clearInterval(counter);
+      timeCount.textContent = "00";
+    }
+  }
+}
+
+function queCounter(index) {
+  const bottom_ques_counter = quiz_box.querySelector(".total-que");
+  let totalQuesCountTag =
+    "<span><p>" +
+    index +
+    "</p>of<p>" +
+    questions.length +
+    "</p>Questions</span>";
+  bottom_ques_counter.innerHTML = totalQuesCountTag;
+}
---
+++ js/questions.js
@@ -0,0 +1,125 @@
+let questions = [
+  {
+    num: 1,
+    question: "What is JavaScript?",
+    answer:
+      "JavaScript is a scripting language used to make the website interactive",
+    options: [
+      "JavaScript is an assembly language used to make the website interactive",
+      "JavaScript is a scripting language used to make the website interactive",
+      "JavaScript is a compiled language used to make the website interactive",
+      "Hyper Tool Multiple Language",
+    ],
+  },
+
+  {
+    num: 2,
+    question: "Which of the following is correct about JavaScript?",
+    answer: "JavaScript is an Object-Based language",
+    options: [
+      "JavaScript is Assembly-language",
+      "JavaScript is a scripting language used to make the website interactive",
+      "JavaScript is an Object-Based language",
+      "JavaScript is an Object-Oriented language",
+    ],
+  },
+
+  {
+    num: 3,
+    question:
+      "Among the given statements, which statement defines closures in JavaScript?",
+    answer:
+      "JavaScript is a function that is enclosed with references to its lexical environment",
+    options: [
+      "JavaScript is Assembly-language",
+      "JavaScript is a scripting language used to make the website interactive",
+      "JavaScript is a function that is enclosed with references to its lexical environment",
+      " None of the mentioned",
+    ],
+  },
+
+  {
+    num: 10,
+    question: "What does HTML stand for",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multiple Language",
+    ],
+  },
+  {
+    num: 12,
+    question: "What is HTML?",
+    answer: "All of the mentioned ",
+    options: [
+      "HTML describes the structure of a webpage",
+      "HTML is the standard markup language mainly used to create web pages",
+      "HTML consists of a set of elements that helps the browser how to view the content",
+      "All of the mentioned ",
+    ],
+  },
+  {
+    num: 13,
+    question: "Who is the father of HTML?",
+    answer: "Tim Berners-Lee",
+    options: [
+      "Rasmus Lerdorf",
+      "Tim Berners-Lee",
+      "Brendan Eich",
+      "Sergey Brin",
+    ],
+  },
+  {
+    num: 14,
+    question:
+      "Which of the following is used to read an HTML page and render it?",
+    answer: "Web browser",
+    options: ["Web server", " Web network", "Web browser", " Web matrix"],
+  },
+  {
+    num: 15,
+    question:
+      "Which of the following elements in HTML5 defines video or movie content?",
+    answer: "video",
+    options: ["movie", " audio", " Web browser", "video"],
+  },
+  {
+    num: 16,
+    question:
+      "Which of the following is not the element associated with the HTML table layout?",
+    answer: "color",
+    options: ["movie", "color", " Web browser", "audio"],
+  },
+  {
+    num: 17,
+    question: "How do we write comments in HTML?",
+    answer: "!……",
+    options: ["!... ", "!……", "/……/", " …….!"],
+  },
+  {
+    num: 18,
+    question: "In which part of the HTML metadata is contained? ",
+    answer: "head tag",
+    options: [" title tag", "head tag", "  html tag", " b"],
+  },
+  {
+    num: 19,
+    question:
+      "Which of the following tag is used for inserting the largest heading in HTML?",
+    answer: "h1",
+    options: ["comments", "  head ", "h1", " h6 "],
+  },
+  {
+    num: 20,
+    question: "What is the correct syntax of doctype in HTML5?",
+    answer: "!doctype html",
+    options: [
+      "/doctype html",
+      " doctype html!",
+      "!doctype html",
+      " doctype html",
+    ],
+  },
+];
